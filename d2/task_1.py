6c116bfc058c93c2bfa90a50fe816db573b4e259
Iver Oknes <iver@oknes.no>
2019-12-03 18:16:58 +0100

    Added files for day 2 solution.

--- d2/task_1.py
+++ d2/task_1.py
@@ -1,3 +1,28 @@
 """
 Advent of code 2019: Day 2, task 1
 """
+
+import numpy as np
+
+file = open("input.txt", "r")
+file = np.array(file.readlines()[0].split(",")).astype("int")
+
+file[1] = 12
+file[2] = 2
+
+ptr = 0
+
+while True:
+    print(ptr)
+    if file[ptr] == 1:
+        file[file[ptr + 3]] = file[file[ptr + 1]] + file[file[ptr + 2]]
+        ptr += 4
+    elif file[ptr] == 2:
+        file[file[ptr + 3]] = file[file[ptr + 1]] * file[file[ptr + 2]]
+        ptr += 4
+    elif file[ptr] == 99:
+        print("Halt code reached. Finished with:")
+        print(file[0])
+        exit()
+    else:
+        raise Exception("Opcode error!")
--- d2/task_2.py
+++ d2/task_2.py
@@ -1,3 +1,35 @@
 """
 Advent of code 2019: Day 2, task 2
 """
+
+import numpy as np
+
+file = open("input.txt", "r")
+file = np.array(file.readlines()[0].split(",")).astype("int")
+
+file_cpy = file.copy()
+last_output = 0
+
+for noun in range(0, 100):
+    for verb in range(0, 100):
+        ptr = 0
+        file = file_cpy.copy()
+        file[1] = noun
+        file[2] = verb
+        while True:
+            if file[ptr] == 1:
+                file[file[ptr + 3]] = file[file[ptr + 1]] + file[file[ptr + 2]]
+            elif file[ptr] == 2:
+                file[file[ptr + 3]] = file[file[ptr + 1]] * file[file[ptr + 2]]
+            elif file[ptr] == 99:
+                print("Halt code reached. Finished with:")
+                last_output = file[0]
+                print(f"@ ptr: {100 * noun + verb}")
+                if last_output == 19690720:
+                    print(noun * 100 + verb)
+                    exit()
+                break
+            else:
+                raise Exception("Opcode error!")
+            ptr += 4
+print("List exceeded")
